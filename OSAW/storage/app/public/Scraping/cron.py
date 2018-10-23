75886f88ba97155d65ab726902f129239904d56e
Jesus Guijarro <jfgs1992@gmail.com>
2018-10-23 14:52:59 +0200

    Crontab-  quitado herramientas y quincenal

--- OSAW/storage/app/public/Scraping/cron.py
+++ OSAW/storage/app/public/Scraping/cron.py
@@ -5,19 +5,18 @@ from datetime import datetime
 
 #Argumentos
 
-operacion=str(sys.argv[1]) # crear = c; modificar = m; eliminar = e
+operacion=str(sys.argv[1]) # Crear -> C; Actualizar -> A; Eliminar -> E
 sitio_id=sys.argv[2]
-herramienta=sys.argv[3]
-periodicidad=int(sys.argv[4]) # Diaria : 1; Semanal : 2; Quincenal : 3; Mensual : 4
-hora_dia=sys.argv[5]
-dia=sys.argv[6] #Día del mes o de la semana - Valor 0 en caso de no necesitar
+periodicidad=str(sys.argv[3]) # Diaria -> D; Semanal -> S;  Mensual -> M
+hora_dia=sys.argv[4]
+dia=sys.argv[5] #Día del mes o de la semana - Valor 0 en caso de no necesitar
 
 #Inicializamos crontab
 cron = CronTab(user='jesus')
 
 #Método para eliminar una tarea
-def eliminar(sitio_id,herramienta):
-    tareas=cron.find_comment(str(sitio_id)+"-"+str(herramienta))  
+def eliminar(sitio_id):
+    tareas=cron.find_comment(comentario)  
 
     for tarea in tareas:
         cron.remove(tarea)  
@@ -25,19 +24,18 @@ def eliminar(sitio_id,herramienta):
     cron.write()
 
 #Método para crear una tarea
-def crear(sitio_id,herramienta,periodicidad,hora_dia,dia):
+def crear(sitio_id,periodicidad,hora_dia,dia):
 
     #Rutas
     directorio = str(os.getcwd())
     directorio=directorio.replace("/Scraping","")
     directorio=directorio.replace("/storage/app","")
 
-    #ruta_evaluar=directorio+"/storage/Scraping/" + evaluar.py + " " +sitio_id
-    ruta_evaluar=directorio+"/storage/Scraping/test.py"
+    ruta_evaluar=directorio+"/storage/Scraping/evaluar.py " + sitio_id
 
     #Comando y comentario asignado a la tarea
     comando="/usr/bin/python3 "+ ruta_evaluar
-    comentario=str(sitio_id)+"-"+str(herramienta)
+    comentario="evaluar-sitio:"+str(sitio_id)
 
     tarea= cron.new(command=comando, comment=comentario)  
 
@@ -50,23 +48,24 @@ def crear(sitio_id,herramienta,periodicidad,hora_dia,dia):
     tarea.minute.on(int(minuto))
     tarea.hour.on(int(hora))
 
-    if periodicidad == 2: #Semanal
+    if periodicidad == "S": #Semanal
         tarea.dow.on(dia)
-    elif periodicidad == 3: #Quincenal
-        tarea.day.every(14)
-    elif periodicidad == 4: # Mensual
+    elif periodicidad == "M": # Mensual
         tarea.day.on(dia)
 
     cron.write() 
 
 #Método para ejecutar la operacion solicitada
 def realizarOperacion(operacion):
-    if operacion == "c":
-        crear(sitio_id,herramienta,periodicidad,hora_dia,dia)
-    elif operacion == "m":
-        eliminar(sitio_id,herramienta)
-        crear(sitio_id,herramienta,periodicidad,hora_dia,dia)
-    else: 
-        eliminar(sitio_id,herramienta)
+    if operacion == "C":
+        crear(sitio_id,periodicidad,hora_dia,dia)
+    elif operacion == "U":
+        eliminar(sitio_id)
+        crear(sitio_id,periodicidad,hora_dia,dia)
+    elif operacion == "E":
+        eliminar(sitio_id)
+    else:
+        #Error en log.txt
+        return False
 
 realizarOperacion(operacion)
