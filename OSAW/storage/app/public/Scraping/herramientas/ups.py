ce48905295da8e5aab015048a56866dbe99aec77
Jesus Guijarro <jfgs1992@gmail.com>
2018-10-24 01:37:51 +0200

    Crear 2

--- OSAW/storage/app/public/Scraping/herramientas/ups.py
+++ OSAW/storage/app/public/Scraping/herramientas/ups.py
@@ -52,9 +52,131 @@ try:
     porcentaje_perceptible=datos_json["oaw"]["resultado"]["resumen"]["totalPerceptible"]
     porcentaje_robusto=datos_json["oaw"]["resultado"]["resumen"]["totalRobusto"]
 
-    ruta_archivo_datos=directorio+"/storage/"+herramienta+"/"+pagina_id+"_"+fecha
     
     
+    #Crear reporte
+    ruta_reporte=directorio+"/storage/"+herramienta+"/"+pagina_id+"_"+str(fecha)+".txt"
+    reporte = open(ruta_reporte, 'a')
+    
+    #Obtenemos la lista de principios que es la única correcta 
+    lista=datos_json["oaw"]["resultado"]["principios"]
+
+    #Los isinstance para comprar que el elemento es una lista
+    #Los try para ignorar los casos en los que se no se encuentra cierto elemento- Cuando falla el patrón
+    #Cuatro niveles: 1.Principios, 2.Pautas, 3.Criterios, 4.Ténicas
+    reporte.write('Reporte de la página web: ' + pagina_url+ '\t\t'+"Fecha: "+ fecha+'\n')
+    for l in lista:
+        reporte.write('Principio: '+str(l["numero"])+'.'+str(l["titulo"])+'.\n') 
+        reporte.write('Descripcion: ' + str(l["descripcion"])+'\n')
+        pautas=l["pautas"]
+        if isinstance(pautas, list):
+            for p in pautas:
+                reporte.write('\tPauta: ' + str(p["numero"])+'.'+ str(p["titulo"])+'\n')
+                reporte.write('\tDescripcion: ' + str(p["descripcion"])+'\n')
+                try:
+                    criterios=p["criterios"]
+                    if isinstance(criterios, list):
+                        for c in criterios:
+                            reporte.write('\t\tCriterio: ' + str(c["numero"])+'.'+ str(c["titulo"])+'\n')
+                            reporte.write('\t\tNivel: ' + str(c["nivel"])+'\n')
+                            try:
+                                tecnicas= c["tecnicas"]
+                                if isinstance(tecnicas, list):
+                                    for t in tecnicas:
+                                        reporte.write('\t\t\tCódigo: ' + str(t["codigo"])+'-'+ str(t["titulo"])+'\n')
+                                        reporte.write('\t\t\tCriticidad: ' + str(t["criticidad"])+'.\n')
+                                        reporte.write('\t\t\tObservación: ' + str(t["observacion"])+'.\n')
+                                        reporte.write('\t\t\tRecomendación: ' + str(t["recomendacion"])+'.\n')
+                                        print(t["codigo"])
+                                        print(t["criticidad"])
+                                        print(t["titulo"])
+                                        print(t["errores"])
+                                        print(t["observacion"])
+                                        print(t["recomendacion"])
+                                else:
+                                    print(c["tecnicas"]["codigo"])
+                                    print(c["tecnicas"]["criticidad"])
+                                    print(c["tecnicas"]["titulo"])
+                                    print(c["tecnicas"]["errores"])
+                                    print(c["tecnicas"]["observacion"])
+                                    print(c["tecnicas"]["recomendacion"])
+
+                            except Exception as e:
+                                pass
+                    else:
+                        print(p["criterios"]["numero"])
+                        print(p["criterios"]["nivel"])
+                        print(p["criterios"]["titulo"])
+                        try:
+                            tecnicas= p["criterios"]["tecnicas"]
+                            if isinstance(tecnicas, list):
+                                for t in tecnicas:
+                                    pass
+                                    print(t["codigo"])
+                                    print(t["criticidad"])
+                                    print(t["titulo"])
+                                    print(t["errores"])
+                                    print(t["observacion"])
+                                    print(t["recomendacion"])
+                        except Exception as e:
+                            pass
+
+
+                except Exception as e:
+                    pass        
+        else:
+            print(l["pautas"]["numero"])
+            print(l["pautas"]["descripcion"])
+            print(l["pautas"]["titulo"])
+            try:
+                criterios=l["pautas"]["criterios"]
+                if isinstance(criterios, list):
+                    for c in criterios:
+                        print(c["numero"])
+                        print(c["nivel"])
+                        print(c["titulo"])
+                        try:
+                            tecnicas= c["tecnicas"]
+                            if isinstance(tecnicas, list):
+                                for t in tecnicas:
+                                    pass
+                                    print(t["codigo"])
+                                    print(t["criticidad"])
+                                    print(t["titulo"])
+                                    print(t["errores"])
+                                    print(t["observacion"])
+                                    print(t["recomendacion"])
+                            else:
+                                print(c["tecnicas"]["codigo"])
+                                print(c["tecnicas"]["criticidad"])
+                                print(c["tecnicas"]["titulo"])
+                                print(c["tecnicas"]["errores"])
+                                print(c["tecnicas"]["observacion"])
+                                print(c["tecnicas"]["recomendacion"])
+
+                        except Exception as e:
+                            pass
+                else:
+                    print(p["criterios"]["numero"])
+                    print(p["criterios"]["nivel"])
+                    print(p["criterios"]["titulo"])
+                    try:
+                        tecnicas= p["criterios"]["tecnicas"]
+                        if isinstance(tecnicas, list):
+                            for t in tecnicas:
+                                pass
+                                print(t["codigo"])
+                                print(t["criticidad"])
+                                print(t["titulo"])
+                                print(t["errores"])
+                                print(t["observacion"])
+                                print(t["recomendacion"])
+                    except Exception as e:
+                        pass
+            except Exception as e:
+                pass
+
+            
 
 except Exception as e:
 
