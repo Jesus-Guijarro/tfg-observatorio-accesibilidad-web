14ce8d44998594cd63dfb45f48ae178be174b1b9
Jesus Guijarro <jfgs1992@gmail.com>
2018-10-24 02:01:25 +0200

    Crear completado

--- OSAW/storage/app/public/Scraping/herramientas/ups.py
+++ OSAW/storage/app/public/Scraping/herramientas/ups.py
@@ -66,117 +66,106 @@ try:
     #Cuatro niveles: 1.Principios, 2.Pautas, 3.Criterios, 4.Ténicas
     reporte.write('Reporte de la página web: ' + pagina_url+ '\t\t'+"Fecha: "+ fecha+'\n')
     for l in lista:
-        reporte.write('Principio: '+str(l["numero"])+'.'+str(l["titulo"])+'.\n') 
-        reporte.write('Descripcion: ' + str(l["descripcion"])+'\n')
+        reporte.write('\nPrincipio: '+str(l["numero"])+'.'+str(l["titulo"])+'\n') 
+        reporte.write('Descripcion: ' + str(l["descripcion"])+'\n\n')
         pautas=l["pautas"]
         if isinstance(pautas, list):
             for p in pautas:
                 reporte.write('\tPauta: ' + str(p["numero"])+'.'+ str(p["titulo"])+'\n')
-                reporte.write('\tDescripcion: ' + str(p["descripcion"])+'\n')
+                reporte.write('\tDescripcion: ' + str(p["descripcion"])+'\n\n')
                 try:
                     criterios=p["criterios"]
                     if isinstance(criterios, list):
                         for c in criterios:
                             reporte.write('\t\tCriterio: ' + str(c["numero"])+'.'+ str(c["titulo"])+'\n')
-                            reporte.write('\t\tNivel: ' + str(c["nivel"])+'\n')
+                            reporte.write('\t\tNivel: ' + str(c["nivel"])+'\n\n')
                             try:
                                 tecnicas= c["tecnicas"]
                                 if isinstance(tecnicas, list):
                                     for t in tecnicas:
                                         reporte.write('\t\t\tCódigo: ' + str(t["codigo"])+'-'+ str(t["titulo"])+'\n')
-                                        reporte.write('\t\t\tCriticidad: ' + str(t["criticidad"])+'.\n')
-                                        reporte.write('\t\t\tObservación: ' + str(t["observacion"])+'.\n')
-                                        reporte.write('\t\t\tRecomendación: ' + str(t["recomendacion"])+'.\n')
-                                        print(t["codigo"])
-                                        print(t["criticidad"])
-                                        print(t["titulo"])
-                                        print(t["errores"])
-                                        print(t["observacion"])
-                                        print(t["recomendacion"])
+                                        reporte.write('\t\t\tCriticidad: ' + str(t["criticidad"])+'\n')
+                                        reporte.write('\t\t\tObservación: ' + str(t["observacion"])+'\n')
+                                        reporte.write('\t\t\tRecomendación: ' + str(t["recomendacion"])+'\n')
+                                        reporte.write('\t\t\tNúmero de errores: ' + str(t["errores"])+'\n\n')
                                 else:
-                                    print(c["tecnicas"]["codigo"])
-                                    print(c["tecnicas"]["criticidad"])
-                                    print(c["tecnicas"]["titulo"])
-                                    print(c["tecnicas"]["errores"])
-                                    print(c["tecnicas"]["observacion"])
-                                    print(c["tecnicas"]["recomendacion"])
-
+                                    reporte.write('\t\t\tCódigo: ' + str(c["tecnicas"]["codigo"])+'-'+ str(c["tecnicas"]["titulo"])+'\n')
+                                    reporte.write('\t\t\tCriticidad: ' + str(c["tecnicas"]["criticidad"])+'\n')
+                                    reporte.write('\t\t\tObservación: ' + str(c["tecnicas"]["observacion"])+'\n')
+                                    reporte.write('\t\t\tRecomendación: ' + str(c["tecnicas"]["recomendacion"])+'\n')
+                                    reporte.write('\t\t\tNúmero de errores: ' + str(c["tecnicas"]["errores"])+'\n\n')
                             except Exception as e:
                                 pass
                     else:
-                        print(p["criterios"]["numero"])
-                        print(p["criterios"]["nivel"])
-                        print(p["criterios"]["titulo"])
+                        reporte.write('\t\tCriterio: ' + str(p["criterios"]["numero"])+'.'+ str(p["criterios"]["titulo"])+'\n')
+                        reporte.write('\t\tNivel: ' + str(p["criterios"]["nivel"])+'\n\n')
                         try:
                             tecnicas= p["criterios"]["tecnicas"]
                             if isinstance(tecnicas, list):
                                 for t in tecnicas:
-                                    pass
-                                    print(t["codigo"])
-                                    print(t["criticidad"])
-                                    print(t["titulo"])
-                                    print(t["errores"])
-                                    print(t["observacion"])
-                                    print(t["recomendacion"])
+                                    reporte.write('\t\t\tCódigo: ' + str(t["codigo"])+'-'+ str(t["titulo"])+'\n')
+                                    reporte.write('\t\t\tCriticidad: ' + str(t["criticidad"])+'\n')
+                                    reporte.write('\t\t\tObservación: ' + str(t["observacion"])+'\n')
+                                    reporte.write('\t\t\tRecomendación: ' + str(t["recomendacion"])+'\n')
+                                    reporte.write('\t\t\tNúmero de errores: ' + str(t["errores"])+'\n\n')
+                            else:
+                                reporte.write('\t\t\tCódigo: ' + str(c["tecnicas"]["codigo"])+'-'+ str(c["tecnicas"]["titulo"])+'\n')
+                                reporte.write('\t\t\tCriticidad: ' + str(c["tecnicas"]["criticidad"])+'\n')
+                                reporte.write('\t\t\tObservación: ' + str(c["tecnicas"]["observacion"])+'\n')
+                                reporte.write('\t\t\tRecomendación: ' + str(c["tecnicas"]["recomendacion"])+'\n')
+                                reporte.write('\t\t\tNúmero de errores: ' + str(c["tecnicas"]["errores"])+'\n\n')
                         except Exception as e:
                             pass
-
-
                 except Exception as e:
                     pass        
         else:
-            print(l["pautas"]["numero"])
-            print(l["pautas"]["descripcion"])
-            print(l["pautas"]["titulo"])
+            reporte.write('\tPauta: '+str(l["pautas"]["numero"])+'.'+str(l["pautas"]["titulo"])+'\n') 
+            reporte.write('Descripcion: ' + str(l["pautas"]["descripcion"])+'\n\n')
             try:
                 criterios=l["pautas"]["criterios"]
                 if isinstance(criterios, list):
                     for c in criterios:
-                        print(c["numero"])
-                        print(c["nivel"])
-                        print(c["titulo"])
+                        reporte.write('\t\tCriterio: ' + str(c["numero"])+'.'+ str(c["titulo"])+'\n')
+                        reporte.write('\t\tNivel: ' + str(c["nivel"])+'\n\n')
                         try:
                             tecnicas= c["tecnicas"]
                             if isinstance(tecnicas, list):
                                 for t in tecnicas:
-                                    pass
-                                    print(t["codigo"])
-                                    print(t["criticidad"])
-                                    print(t["titulo"])
-                                    print(t["errores"])
-                                    print(t["observacion"])
-                                    print(t["recomendacion"])
+                                    reporte.write('\t\t\tCódigo: ' + str(t["codigo"])+'-'+ str(t["titulo"])+'\n')
+                                    reporte.write('\t\t\tCriticidad: ' + str(t["criticidad"])+'\n')
+                                    reporte.write('\t\t\tObservación: ' + str(t["observacion"])+'\n')
+                                    reporte.write('\t\t\tRecomendación: ' + str(t["recomendacion"])+'\n')
+                                    reporte.write('\t\t\tNúmero de errores: ' + str(t["errores"])+'\n\n')
                             else:
-                                print(c["tecnicas"]["codigo"])
-                                print(c["tecnicas"]["criticidad"])
-                                print(c["tecnicas"]["titulo"])
-                                print(c["tecnicas"]["errores"])
-                                print(c["tecnicas"]["observacion"])
-                                print(c["tecnicas"]["recomendacion"])
-
+                                reporte.write('\t\t\tCódigo: ' + str(c["tecnicas"]["codigo"])+'-'+ str(c["tecnicas"]["titulo"])+'\n')
+                                reporte.write('\t\t\tCriticidad: ' + str(c["tecnicas"]["criticidad"])+'\n')
+                                reporte.write('\t\t\tObservación: ' + str(c["tecnicas"]["observacion"])+'\n')
+                                reporte.write('\t\t\tRecomendación: ' + str(c["tecnicas"]["recomendacion"])+'\n')
+                                reporte.write('\t\t\tNúmero de errores: ' + str(c["tecnicas"]["errores"])+'\n\n')
                         except Exception as e:
                             pass
                 else:
-                    print(p["criterios"]["numero"])
-                    print(p["criterios"]["nivel"])
-                    print(p["criterios"]["titulo"])
+                    reporte.write('\t\tCriterio: ' + str(p["criterios"]["numero"])+'.'+ str(p["criterios"]["titulo"])+'\n')
+                    reporte.write('\t\tNivel: ' + str(p["criterios"]["nivel"])+'\n\n')
                     try:
                         tecnicas= p["criterios"]["tecnicas"]
                         if isinstance(tecnicas, list):
                             for t in tecnicas:
-                                pass
-                                print(t["codigo"])
-                                print(t["criticidad"])
-                                print(t["titulo"])
-                                print(t["errores"])
-                                print(t["observacion"])
-                                print(t["recomendacion"])
+                                reporte.write('\t\t\tCódigo: ' + str(t["codigo"])+'-'+ str(t["titulo"])+'\n')
+                                reporte.write('\t\t\tCriticidad: ' + str(t["criticidad"])+'\n')
+                                reporte.write('\t\t\tObservación: ' + str(t["observacion"])+'\n')
+                                reporte.write('\t\t\tRecomendación: ' + str(t["recomendacion"])+'\n')
+                                reporte.write('\t\t\tNúmero de errores: ' + str(t["errores"])+'\n\n')
+                        else:
+                            reporte.write('\t\t\tCódigo: ' + str(c["tecnicas"]["codigo"])+'-'+ str(c["tecnicas"]["titulo"])+'\n')
+                            reporte.write('\t\t\tCriticidad: ' + str(c["tecnicas"]["criticidad"])+'\n')
+                            reporte.write('\t\t\tObservación: ' + str(c["tecnicas"]["observacion"])+'\n')
+                            reporte.write('\t\t\tRecomendación: ' + str(c["tecnicas"]["recomendacion"])+'\n')
+                            reporte.write('\t\t\tNúmero de errores: ' + str(c["tecnicas"]["errores"])+'\n\n')
                     except Exception as e:
                         pass
             except Exception as e:
                 pass
-
-            
 
 except Exception as e:
 
